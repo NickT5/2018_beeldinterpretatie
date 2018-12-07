8d10a48ebcb9d639d691f88fa067d86f3ab8742a
Nick Todts <nick.todts@hotmail.com>
2018-12-07 08:42:06 +0100

    Running the video by showing it frame by frame.

--- sessie_6_1/main.cpp
+++ sessie_6_1/main.cpp
@@ -45,10 +45,11 @@ int main(int argc, char** argv)
     CascadeClassifier detectorHAAR(HAAR_location);
     CascadeClassifier detectorLBP(LBP_location);
 
-    ///Load the HAAR and LBP classifier using the
+    ///Load the HAAR and LBP classifier.
     detectorHAAR.load(HAAR_location);
     detectorLBP.load(LBP_location);
 
+    ///Check if classifiers are successfully loaded.
     if( detectorHAAR.empty() ){
         cerr << "Failed to load the HAAR classifier!" << endl;
         return -1;
@@ -60,6 +61,30 @@ int main(int argc, char** argv)
         return -1;
     }
     else{ cout << "Successfully loaded the LBP CLF." << endl; }
+
+
+    Mat frame;
+    ///Take a frame from the video capture each iteration until ESC is pressed.
+    while( true ){
+        ///Take a frame from the videocapture.
+        video >> frame;
+
+        ///Check wether the frame is empty.
+        ///Possible when there's a problem with the frame or the end of the video is reached.
+        if(frame.empty()){
+            cerr << "EMPTY FRAME!" << endl;
+            cout << "Possible when there's a problem with the frame or the end of the video is reached." << endl;
+            return 0;
+        }
+
+        imshow("Frame", frame);
+
+        ///Press ESC on keyboard to exit.
+        char c = (char)waitKey(25);
+        if(c==27){
+            break;
+        }
+    }
 
 
     /// When everything done, release the video capture object
