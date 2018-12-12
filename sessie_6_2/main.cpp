f381921e001e0d05705588d9585660de8e1722c4
Nick Todts <nick.todts@hotmail.com>
2018-12-12 11:38:15 +0100

    Add a person detector. Visualize bounding box with score and a tracking line.

--- sessie_6_2/main.cpp
+++ sessie_6_2/main.cpp
@@ -39,7 +39,16 @@ int main(int argc, char** argv)
 
     //video.set(CAP_PROP_POS_FRAMES, 10);       ///Skip x-frames of the video.
 
+    ///Create a list of points to store the middlepoints of the bounding box from the detection.
+    vector<Point> middle;
+
+    ///Create HOG people detector.
+    HOGDescriptor hog;
+    hog.setSVMDetector(HOGDescriptor::getDefaultPeopleDetector());
+
     Mat frame;
+    Mat canvas;
+
     ///Take a frame from the video capture each iteration until ESC is pressed.
     while( true ){
         ///Take a frame from the videocapture.
@@ -53,16 +62,46 @@ int main(int argc, char** argv)
             return 0;
         }
 
+        ///Double the size.
+        resize(frame, frame, Size(), 2, 2);
 
-        imshow("Frame", frame);
+        ///Use the detector to detect the person in the frame.
+        vector<Rect> people;
+        vector<double> weights;
+        hog.detectMultiScale(frame, people, weights);
 
-        ///Press ESC on keyboard to exit.
+        canvas = frame.clone();
+
+        ///Draw the rectangle and output the scoretext.
+        for(unsigned i = 0; i < people.size(); i++){
+            ///Haal het middelpunt op. Straks gebruiken voor het tracken.
+            middle.push_back(Point(people[i].x+people[i].width/2, people[i].y+people[i].height/2));
+
+            ///Draw a bounding box on the canvas.
+            rectangle(canvas, people[i], Scalar(255, 255, 0));
+
+            ///Draw text (=score) on the canvas.
+            stringstream temp;
+            temp << (double)weights[i];
+            putText(canvas, temp.str(), Point(people[i].x, people[i].y), FONT_HERSHEY_SIMPLEX, 1, Scalar(255, 255, 0));
+        }
+
+        ///Draw the tracking lines.
+        for(unsigned j = 1; j < middle.size(); j++){
+                ///Lijn tekenen tussen de vorige en de huidige punt in een rode kleur.
+                line(canvas, middle[j-1], middle[j], Scalar(0, 0, 255));
+        }
+
+        ///Show the canvas.
+        //imshow("Frame", frame);
+        imshow("Canvas", canvas);
+
+        ///Press ESC on the keyboard to exit.
         char c = (char)waitKey(30);
         if(c==27){
             break;
         }
     }
-
 
     /// When everything done, release the video capture object
     video.release();
