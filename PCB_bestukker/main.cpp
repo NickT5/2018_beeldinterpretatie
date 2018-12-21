34e23ddd78a0a5a8f7c3756284dfa68028d6ae59
Nick Todts <nick.todts@hotmail.com>
2018-12-21 12:44:54 +0100

    Init project: PCB_bestukker

---
+++ PCB_bestukker/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, const char **argv)
+{
+    cout << "Project: PCB bestukker." << endl;
+
+    return 0;
+}
