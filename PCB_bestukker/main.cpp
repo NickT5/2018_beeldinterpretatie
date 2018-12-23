abe811a6006bc9a947814c6ab8f9b9cdb4077e8d
Nick Todts <nick.todts@hotmail.com>
2018-12-23 12:43:57 +0100

    Template matching with a bounding box (for 1 object)

--- PCB_bestukker/main.cpp
+++ PCB_bestukker/main.cpp
@@ -46,6 +46,42 @@ int main(int argc, const char **argv)
     waitKey(0);
 
 
+    ///Clone input images.
+    Mat pcb = inputImages[0].clone();
+    Mat templ = inputImages[1].clone();
+
+    ///Create Mat object for the result.
+    Mat matchResult = Mat::zeros(pcb.rows, pcb.cols, CV_8UC1);
+
+    ///Template matching
+    int match_method[] = {CV_TM_SQDIFF, CV_TM_SQDIFF_NORMED, CV_TM_CCORR, CV_TM_CCORR_NORMED, CV_TM_CCOEFF, CV_TM_CCOEFF_NORMED};
+
+    matchTemplate(pcb, templ, matchResult, match_method[3]);
+    imshow("Result (single) template match", matchResult);
+    waitKey(0);
+
+    ///Met normalize() kan je ervoor zorgen dat de min waarde -> 0 en max -> 1.
+    ///Dit zorgt ervoor dat het template match result duidelijker wordt.
+    normalize( matchResult, matchResult, 0, 1, NORM_MINMAX, -1, Mat() );
+    imshow("TM Normalized", matchResult);
+    waitKey(0);
+
+    ///Bounding box:
+    ///In matchResult is het meer wit waar een gevonden match is en anders is het zwart.
+    ///We kunnen dus minMaxLoc() gebruiken.
+    ///minMaxLoc() finds the global min and max in an array
+    double minVal; double maxVal;   //min en max value.
+    Point minLoc; Point maxLoc;    //Locatie waar de min en max gevonden zijn.
+    minMaxLoc(matchResult, &minVal, &maxVal, &minLoc, &maxLoc, Mat() );
+
+    /// 1 bounding box tekenen:
+    Point oppositeCorner = Point(maxLoc.x+templ.cols,maxLoc.y+templ.rows);
+    Mat result = inputImages[0].clone();
+    rectangle(result, maxLoc, oppositeCorner, Scalar(0,0,255));
+
+    imshow("Result with bounding box",result);
+    waitKey(0);
+
 
 
 
